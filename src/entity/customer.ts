693eed3dc34e818b2665a6a8d8cc81f562000c7d
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-12-05 15:07:46 -0300

    Diferenca de entidade de negocio para entidade de persistencia

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,38 +1,55 @@
 // Entidade anemica (sem comportamento)
+// Uma entidade por padrão sempre tem que se auto validar e garantir que possua dados válidos
+// Uma entidade por padrão sempre tem que ter um id
+// Entidade focada em negocio, não em persistencia
+
+// Pare de chamar sua classe usada para persistencia de entidade
+// Entidade é uma classe que representa um conceito de negocio
+
+// Complexidade de negocio
+// Domain
+// - Entity
+// - - customer.ts (regra de negocio)
+
+// Complexidade acidental
+// Infra - Mundo externo (banco de dados, api, etc)
+// - Entity / Model
+// - - customer.ts (persistencia)
 class Customer {
 
     private _id: string;
     private _name: string;
-    private _address: string;
+    private _address: string = '';
+    private _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
     }
 
-
-    get id(): string {
-        return this._id;
+    validate() {
+        if (this._name.length < 5) {
+            throw new Error('Name is required');
+        }
+        if (this._id.length === 0) {
+            throw new Error('Id is required');
+        }
     }
 
-    set id(value: string) {
-        this._id = value;
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
     }
 
-    get name(): string {
-        return this._name;
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error('Address is mandatory to activate customer');
+        }
+        this._active = true;
     }
 
-    set name(value: string) {
-        this._name = value;
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set address(value: string) {
-        this._address = value;
+    deactivate() {
+        this._active = false;
     }
 }
