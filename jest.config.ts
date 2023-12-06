d40307e0fe3407ff7f552028e92adb94773c0480
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-12-06 13:54:07 -0300

    Criado testes unitarios de customer e address

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,13 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+export default {
+  transform: {
+    "^.+\.(t|j)sx?$": ["@swc/jest"],
 
-const config: Config = {
+  },
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -13,9 +15,9 @@ const config: Config = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/9f/r1tyq4mn793858b_nkd8shn40000gq/T/jest_dz",
+  // cacheDirectory: "/private/var/folders/h_/n9cr86t52279b8msmjm777z00000gn/T/jest_dx",
 
-  // Automatically clear mock calls, instances, contexts and results before every test
+  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
@@ -52,11 +54,6 @@ const config: Config = {
   // Make calling deprecated APIs throw helpful error messages
   // errorOnDeprecated: false,
 
-  // The default configuration for fake timers
-  // fakeTimers: {
-  //   "enableGlobally": false
-  // },
-
   // Force coverage collection from ignored files using an array of glob patterns
   // forceCoverageMatch: [],
 
@@ -80,8 +77,6 @@ const config: Config = {
   // An array of file extensions your modules use
   // moduleFileExtensions: [
   //   "js",
-  //   "mjs",
-  //   "cjs",
   //   "jsx",
   //   "ts",
   //   "tsx",
@@ -174,6 +169,12 @@ const config: Config = {
   // This option allows use of a custom test runner
   // testRunner: "jest-circus/runner",
 
+  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+  // testURL: "http://localhost",
+
+  // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
+  // timers: "real",
+
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
@@ -195,5 +196,3 @@ const config: Config = {
   // Whether to use watchman for file crawling
   // watchman: true,
 };
-
-export default config;
