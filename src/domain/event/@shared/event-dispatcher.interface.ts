4b1ad32c2ef0c45dafedeb9387dc97e1051a307c
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-01-20 18:52:23 -0300

    Implementando interfaces Domain Events

---
+++ src/domain/event/@shared/event-dispatcher.interface.ts
@@ -0,0 +1,9 @@
+import EventInterface from "./event.interface";
+
+export default interface EventHandlerInterface {
+
+    notify(event: EventInterface): void;
+    register(eventName: string, eventHandler: EventHandlerInterface): void;
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void;
+    unregisterAll(): void;
+}
