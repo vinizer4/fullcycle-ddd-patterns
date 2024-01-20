47ad6a54809c9d6917664046951502ba62ec7b6c
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-01-20 19:24:12 -0300

    Desregistrando eventos

--- src/domain/event/@shared/impl/event-dispatcher.ts
+++ src/domain/event/@shared/impl/event-dispatcher.ts
@@ -18,12 +18,22 @@ export default class EventDispatcher implements EventDispatcherInterface {
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+        if (!this.eventHandlers[eventName]) {
+            return;
+        }
+
+        const index = this.eventHandlers[eventName].indexOf(eventHandler);
+        if (index !== -1) {
+            this.eventHandlers[eventName].splice(index, 1);
+        }
     }
 
     unregisterAll(): void {
+        this.eventHandlers = {};
     }
 
     notify(event: EventInterface): void {
+
     }
 
 }
